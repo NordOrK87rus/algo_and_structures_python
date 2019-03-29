623364eb1cf72b26fa674ff0031b1e6dc91b295c
nordork <nordork@gmail.com>
2019-03-29 12:06:38 +0300

    fix lesson7 hw1

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,22 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+from timeit import timeit
+from random import randint
+
+
+def bubble_sorting(data):
+    d_len = len(data)
+    for i in range(d_len - 1):
+        for j in range(d_len - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+    return data
+
+
+data_list = [randint(-100, 100) for _ in range(500)]
+
+print(timeit('bubble_sorting(data_list)',
+             setup='from __main__ import bubble_sorting, data_list',
+             number=100))
+
