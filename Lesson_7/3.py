154e45db334457d62660ef43e6256bc4b28e73c3
nordork <nordork@gmail.com>
2019-03-29 12:48:17 +0300

    fix lesson7 hw3

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,24 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+from random import randint
+from timeit import timeit
+
+m = int(input("Введите натуральное число: "))
+arr = [randint(-m, m) for _ in range(m * 2 + 1)]
+
+
+def med(data):
+    n = len(data)
+    if n < 1:
+        return None
+    elif n % 2 == 1:
+        return sorted(data)[n // 2]
+    else:
+        return sum(sorted(data)[n // 2 - 1:n // 2 + 1]) / 2.0
+
+
+print(f"Исходный массив:\t{arr}")
+print(f"Медиана:\t\t\t{med(arr)}")
+print(f"Время обработки:\t{timeit('med(arr)', setup='from __main__ import med, arr',number=100)}")
