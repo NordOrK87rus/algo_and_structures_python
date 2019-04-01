de5b48b7832cd8276e82122fea5ebd6e656fda55
nordork <nordork@gmail.com>
2019-04-01 16:57:44 +0300

    fix lesson8 hw2(1)

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,21 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+
+# s = input("Введите строку: ")
+s = "pasta"
+result = set()
+
+for i in range(len(s)):
+    if i == 0:
+        n = len(s) - 1
+    else:
+        n = len(s)
+
+    for j in range(n, i, -1):
+        # substr.add(hash(str_in[i:j]))
+        result.add(s[i:j])
+
+
+# <class 'set'>: {'p', 'asta', 'ta', 'st', 'sta', 's', 'a', 'ast', 'pa', 'past', 'as', 't', 'pas'} len=13
+# print(f"Количество различных подстрок в строке: {len(substr)}")
